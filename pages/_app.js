f1a1ea7a9f71a1aa4688b1fa93ccb12abd6bfea2
Mohd Monish <mohd2002monish@Mohds-MacBook-Air.local>
2025-08-12 23:38:23 +0530

    Fix CSS import paths and add test page for Vercel debugging

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,23 @@
-import Preloader from "@/components/elements/Preloader"
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-import { useEffect, useState } from "react"
-import 'swiper/css'
-import 'swiper/css/free-mode'
-import "swiper/css/navigation"
-import "swiper/css/pagination"
-import "../public/assets/css/animate.css"
-// import "/public/css/dist/aos.css"
-import "../public/assets/css/app.css"
+import Preloader from "@/components/elements/Preloader";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect, useState } from "react";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "../public/assets/css/animate.css";
+import "../public/assets/css/app.css";
 
 function MyApp({ Component, pageProps }) {
-
-    const [loading, setLoading] = useState(true)
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
         setTimeout(() => {
-            setLoading(false)
-        }, 1000)
-        AOS.init()
-    }, [])
-    return (
-        <div className="home-main header-fixed">
-            {!loading ? (
-                <Component {...pageProps} />
-            ) : (
-                <Preloader />
-            )}
-        </div>)
+            setLoading(false);
+        }, 1000);
+        AOS.init();
+    }, []);
+    return <div className="home-main header-fixed">{!loading ? <Component {...pageProps} /> : <Preloader />}</div>;
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/test.js
@@ -0,0 +1,9 @@
+export default function Test() {
+    return (
+        <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
+            <h1>Test Page Working!</h1>
+            <p>If you can see this, Next.js is working on Vercel.</p>
+            <p>Current time: {new Date().toLocaleString()}</p>
+        </div>
+    );
+}
